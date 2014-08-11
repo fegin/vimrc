797486587e889009c27f64bd5dd134571604f42e
Chien-Chin Huang <fegins@gmail.com>
2014-08-11 15:35:04 -0400

    Update vimproc install

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
 cd -
+cd ~/.vim/bundle/vimproc.vim
+make
+cd -
 
 # brew install cmake
 # sudo apt-get isntall build-essential cmake python-dev
